1cdce25b6effc6c1a8fa46f1cf93a72e75a7fdfc
Mayia <yashin5@gmail.com>
2023-06-28 08:43:17 -0400

    test

---
+++ Korea Covid/.ipynb_checkpoints/startup-checkpoint.py
@@ -0,0 +1,24 @@
+# systems libraries
+import os
+import warnings
+warnings.filterwarnings("ignore")
+
+# data analysis libraries
+import pandas as pd
+import numpy as np
+from scipy.stats import stats
+from scipy.stats import kendalltau
+from datetime import date
+
+# viz
+import matplotlib.pyplot as plt
+import matplotlib.gridspec as gs
+from matplotlib.ticker import PercentFormatter
+import seaborn as sns
+import plotly
+import plotly.graph_objs as go
+import plotly.express as px
+
+
+# formatting
+import IPython
